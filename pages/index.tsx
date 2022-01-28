52ed32c7ebc59bc1e50edcd796e6330636201a12
potionstory <potionstory@gmail.com>
2022-01-28 14:05:30 +0900

    [BUILD] file delete
    - api > hello

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,17 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
+import type { NextPage } from "next";
+import Head from "next/head";
+import React, { Component } from "react";
 
-const Home: NextPage = () => {
-  return (
-    <div>
-      <Head>
-        <title>TODO + MAP</title>
-        <meta name="description" content="todo + map service" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+const Home: NextPage = () => (
+  <div>
+    <Head>
+      <title>TODO + MAP</title>
+      <meta name="description" content="todo + map service" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
 
-      <main>
-        Home
-      </main>
-    </div>
-  )
-}
+    <main>Home</main>
+  </div>
+);
 
-export default Home
+export default Home;
