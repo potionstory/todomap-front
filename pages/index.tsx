fc0e7eec7e48040b9a1afbf74e09f9a5744fbc6d
potionstory <potionstory@gmail.com>
2022-01-28 10:32:37 +0900

    [BUILD] Next.js install

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>TODO + MAP</title>
+        <meta name="description" content="todo + map service" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        Home
+      </main>
+    </div>
+  )
+}
+
+export default Home
