3cfffa9198f8b2a1cb78118c55420d7ed14400b2
potionstory <potionstory@gmail.com>
2022-02-07 15:49:10 +0900

    [FEAT] module import sort update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,17 @@
-import { AppProps } from "next/app";
 import React from "react";
+import { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
 import Layout from "../components/base/Layout";
 import "../styles/font.css";
+import GlobalStyle from "../styles/global";
 import DebugObserver from "./DebugObserver";
+import "@fortawesome/fontawesome-svg-core/styles.css";
 
 const App = ({ Component, pageProps }: AppProps) => (
   <RecoilRoot>
     <DebugObserver />
     <Layout>
+      <GlobalStyle />
       <Component {...pageProps} />
     </Layout>
   </RecoilRoot>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,19 @@
 import { NextPage } from "next";
 import Head from "next/head";
-import Link from "next/link";
 import React from "react";
+import Date from "../components/date/Date";
+import Todo from "../components/todo/Todo";
 
 const Home: NextPage = () => (
   <>
     <Head>
-      <title>TODO + MAP</title>
+      <title>TODO + MAP: Home</title>
       <meta name="description" content="todo + map service" />
       <link rel="icon" href="/favicon.ico" />
     </Head>
 
-    <section>
-      <span>menu</span>
-      <nav>
-        <Link href="/sign">
-          <a>Sign</a>
-        </Link>
-      </nav>
-    </section>
+    <Date />
+    <Todo />
   </>
 );
 
