cc7c486e298420ddfb17637cc4d6cd13afeaa93a
potionstory <potionstory@gmail.com>
2022-02-07 15:49:35 +0900

    [FEAT] theme color update

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,15 +1,15 @@
 import { ThemeColor } from "../types/global";
 
 export const light: ThemeColor = {
-  mainBg: "#ffffff",
-  mainText: "#121212",
-  subBg: "#eeeeee",
+  mainBg: "#fafafa",
+  mainText: "#040404",
+  subBg: "#efefef",
   primary: "#ffa500",
 };
 
 export const dark: ThemeColor = {
-  mainBg: "#121212",
-  mainText: "#ffffff",
-  subBg: "#212121",
+  mainBg: "#040404",
+  mainText: "#fafafa",
+  subBg: "#141414",
   primary: "#ffb52e",
 };
