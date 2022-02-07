f980b7750f675aecc3bfa5b30306bbf372e9fed8
potionstory <potionstory@gmail.com>
2022-02-07 15:49:58 +0900

    [FEAT] global style update

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,12 +1,15 @@
 import { createGlobalStyle } from "styled-components";
-import { ThemeColor } from "../type/global";
+import { ThemeColor } from "../types/global";
 
 const global = createGlobalStyle`
   html {
+    height: 100%;
     background-color: ${({ theme }: { theme: ThemeColor }) => theme.mainBg};
   }
+  
   body {
     margin: 0;
+    height: 100%;
     padding: 0;
     background-color: ${({ theme }: { theme: ThemeColor }) => theme.mainBg};
     font-family: "Barlow", -apple-system, BlinkMacSystemFont, "Helvetica Neue", "Apple SD Gothic Neo", "Malgun Gothic", "맑은 고딕", 나눔고딕, "Nanum Gothic", "Noto Sans KR", "Noto Sans CJK KR", arial, 돋움, Dotum, Tahoma, Geneva, sans-serif;
@@ -15,22 +18,26 @@ const global = createGlobalStyle`
     -moz-osx-font-smoothing: grayscale;
     color: ${({ theme }: { theme: ThemeColor }) => theme.mainText};
   }
+
   * {
     box-sizing: inherit;
   }
+
   h1, h2, h3, h4, h5, h6 {
     margin: 0;
   }
+
   a {
-    color: ${({ theme }: { theme: ThemeColor }) => theme.mainBg};
-    background-color: ${({ theme }: { theme: ThemeColor }) => theme.primary};
     text-decoration: none;
+    color: ${({ theme }: { theme: ThemeColor }) => theme.mainText};
   }
+
   ul, ol, dl {
     margin: 0;
     padding: 0;
     list-style: none;
   }
+
   p {
     margin: 0;
   }
@@ -38,26 +45,36 @@ const global = createGlobalStyle`
   input, button, textarea {
     border: none;
     background-color: transparent;
-    font-family: inherit;
     padding: 0;
+    font-family: inherit;
     cursor: pointer;
     &:focus {
       outline-style: none;
     }
   }
+
   input::-webkit-outer-spin-button,
   input::-webkit-inner-spin-button {
     -webkit-appearance: none;
     margin: 0;
   }
+
   textarea {
     resize: none;
   }
+
   img {
     width: 100%;
   }
+
   img, button {
     vertical-align: top;
+  }
+
+  // root
+  #__next {
+    display: flex;
+    height: 100%;
   }
 `;
 
